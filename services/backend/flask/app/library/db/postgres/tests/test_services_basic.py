a5a478931f3661b5c5d5ef05b73f65f4cb7a3752
Kha <fanbsb90@gmail.com>
2021-11-15 17:57:52 +0700

    Update

--- services/backend/flask/app/library/db/postgres/tests/test_services_basic.py
+++
@@ -1,8 +0,0 @@
-from app import app
-
-def test_homepage():
-    tester = app.test_client()
-    response = tester.get("/", content_type="html/text")
-
-    assert response.status_code == 200
-    assert b"Hello, World!" in response.data
